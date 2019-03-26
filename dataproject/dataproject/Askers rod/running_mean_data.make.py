01cfbfd01bcb8a9a52c3aaca07a9d901970f7a6a
Asker <hms467@econ.ku.dk>
2019-03-26 20:02:35 +0100

    update

--- dataproject/dataproject/Askers rod/running_mean_data.make.py
+++ dataproject/dataproject/Askers rod/running_mean_data.make.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # set timeframe we want to analyze:
 start = datetime.datetime(2011, 1, 1)
-end = datetime.datetime(2017, 1, 1)
+end = datetime.datetime(2017, 12, 31)
 
 ### Data import ####
 
@@ -18,8 +18,8 @@ d.index = pd.to_datetime(d.index)
 d['year']=d.index.year
 #rename column company to year
 d.columns.rename(['Attributes', 'Company'],inplace  = True)
-#show head
-d.head()
+#show DataFrame
+d
 
 
 
@@ -29,17 +29,31 @@ Because the Danish and American stockexchange are sometimes open on different da
 data is missing for some days when one is open and the other is not
 for these days the NaN (missing value) will be replaced with the value of the previous day
 '''
+# checks for missing values
+np.where(pd.isnull(d))
 
-d.reindex(df.index[::-1]).ffill()
+
+# replaces value with mean of the two surrounding values:
+d.interpolate(inplace = True)
+
+'''
+alternatively values could have been replaced with the value of the previous day
+# replaces missing values with the previous day
+d.fillna(method='ffill',inplace = True)
+'''
+# rechecks for missing values
+np.where(pd.isnull(d))
+
 
 
 
 ### Creating aditional columns ###
 
-# make 200 days running mean :
+# make 200 and 50 days running mean :
 rm_200 = d.rolling(200).mean()['Close']
 rm_50 = d.rolling(50).mean()['Close']
-#checking for nonacepted means (x-coordinates should go no higher than 200 and 50)
+
+#checking for nonacepted means (x-coordinates should go no higher than 198 and 48)
 np.where(pd.isnull(rm_200))
 np.where(pd.isnull(rm_50))
 
