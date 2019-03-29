d55f8c7811708805f4dfeded6e75bfba4404ff4a
Asker <hms467@econ.ku.dk>
2019-03-29 20:17:57 +0100

    function to plot

--- dataproject/dataproject/functions.py
+++ dataproject/dataproject/functions.py
@@ -46,3 +46,31 @@ def download_data_with_runmean(companys=['GOOG'], from_year = 2011 , to_year = 2
     d = d.loc[start_show:]
 
     return d
+
+from bokeh.io import output_notebook, push_notebook,show
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import ColumnDataSource, Select
+from ipywidgets import interact
+
+def plot_clossing(d):
+    company_list = list(d.columns.levels[0])
+    company = list(d.columns.levels[0])[0]
+    companysource = ColumnDataSource(d[company])
+    p = figure(x_axis_type='datetime',title=f'Graph with running means for {company}', \
+        tools="pan,box_zoom,reset,save", y_axis_label='Closing price', x_axis_label='Date')
+
+    p.line(x='Date', y='Close', source=companysource, color = 'blue', legend= 'Closing price')
+    p.line(x='Date', y='rm_200', source=companysource, color='black', line_dash='4 4', legend = '200 days')
+    p.line(x='Date', y='rm_50', source=companysource, color='red', line_dash='4 4',legend = '50 days')
+    p.legend.location = "top_left"
+
+
+    def update_name(company):
+        p.title.text = 'Closing price and running mean of ' + str(company)
+        companysource.data = ColumnDataSource(d[str(company)]).data
+        push_notebook()
+    
+    show(p,notebook_handle=True)
+
+
+    interact(update_name,company=company_list)
