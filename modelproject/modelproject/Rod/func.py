b0b75cfd021506f95efb7cb28ff9d2d06e014388
Asker <hms467@econ.ku.dk>
2019-05-07 16:23:56 +0200

    Mikrovalg

--- modelproject/modelproject/Rod/func.py
+++ modelproject/modelproject/Rod/func.py
@@ -1,9 +1,11 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
 
 def plot(x,y):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     ax.plot(x,y)
 
-
+def hello():
+    print(hello)
     # Makro
