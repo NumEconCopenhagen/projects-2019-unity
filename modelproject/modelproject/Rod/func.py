0bede4a40fc4199c317c0dd6f3865c69f23ddf80
Asker <hms467@econ.ku.dk>
2019-05-09 17:11:04 +0200

    asda

--- modelproject/modelproject/Rod/func.py
+++ modelproject/modelproject/Rod/func.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 import numpy as np
 import sympy as sm
-
 def plot(x,y):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
@@ -16,23 +15,21 @@ def plot2(x,y1,y2):
 
 
 # Micro optimization
-def total_utility(c, beta, theta):
+def total_utility(c, weight, theta):
     '''
     Sums utility for c for multiple years
     c is an array 
     '''
     uts = (c**(1-theta)-1)/(1-theta)
-     #time dicounting: 
-    timeweights = [beta**i for i in range(len(c))]
     # sum of utitity
-    t_u = np.dot(uts,timeweights)
-    
+    t_u = np.dot(uts,weight)
+
     return t_u
 
 def prod(k,l,alpha,b):
-    return (b*k**alpha)*(l**(1-alpha))
+    return (k**alpha)*((b*l)**(1-alpha))
 
-def tot_ut_multiple_sks_quick(sks, k0, l0, n, b, beta, theta, alpha, delta):
+def tot_ut_multiple_sks_quick(sks, k0, l0, n, b, weight, theta, alpha, delta):
     '''
     Finds total utitilty for a set of years with a savingsrate for each year
     '''
@@ -46,10 +43,10 @@ def tot_ut_multiple_sks_quick(sks, k0, l0, n, b, beta, theta, alpha, delta):
     
     y_short = prod(k_short,l_short,alpha,b)
     
-    return total_utility(y_short*(1-sks)/l_short, beta, theta)
+    return total_utility(y_short*(1-sks)/l_short, weight, theta)
 
-def optimal_sks(t, b, l0, n, beta, delta, alpha, theta, k0, first=True):
-    obj = lambda sks: -tot_ut_multiple_sks_quick(sks, k0, l0, n, b, beta, theta, alpha, delta)
+def optimal_sks(t, b, l0, n, weight, delta, alpha, theta, k0, first=True):
+    obj = lambda sks: -tot_ut_multiple_sks_quick(sks, k0, l0, n, b, weight, theta, alpha, delta)
     sks0 = np.linspace(alpha,0,t)
 
     bounds = np.full((t,2),[0,1])
